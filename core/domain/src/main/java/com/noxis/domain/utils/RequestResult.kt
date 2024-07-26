1e2975084a2e21b6c30c1cb9b7a7b6cbb1c75336
Tumanov Vycheslav <63115274+NoXis82@users.noreply.github.com>
2024-07-26 18:12:17 +0300

    add action

--- core/domain/src/main/java/com/noxis/domain/utils/RequestResult.kt
+++ core/domain/src/main/java/com/noxis/domain/utils/RequestResult.kt
@@ -3,6 +3,9 @@ package com.noxis.domain.utils
 typealias RootError = Error
 
 sealed interface RequestResult<out D, out E : RootError> {
-    data class Success<out D, out E : RootError>(val data: D) : RequestResult<D, E>
-    data class Error<out D, out E : RootError>(val error: E) : RequestResult<D, E>
+    data class Success<out D, out E : RootError>(val data: D) :
+        RequestResult<D, E>
+
+    data class Error<out D, out E : RootError>(val error: E) :
+        RequestResult<D, E>
 }
